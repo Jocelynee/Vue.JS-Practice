5aed6040ee095094e53a2774fa6245815ac54706
Jocelynee <56833363+Jocelynee@users.noreply.github.com>
2019-11-07 21:40:15 -0500

    add color variants to product

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ var app = new Vue({
   data:{
     product: 'Socks',
     description: 'Great socks!',
-    image: './assets/vmSocks-green.jpg'
+    image: './assets/vmSocks-green.jpg',
+    inventory: 0,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue"
+      }
+    ]
   }
 })
